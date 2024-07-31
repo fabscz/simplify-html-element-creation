fbfa8f7b789df6491eb40f995d6a1e26f6574a0e
fabscz <84963147+fabscz@users.noreply.github.com>
2024-07-31 13:14:10 +0000

    started writing the function. a lot more html properties will follow

--- createHtmlElm.js
+++ createHtmlElm.js
@@ -1,1 +1,30 @@
+function createHtmlElm(properties) {
 
+    var elm = document.createElement(properties.tag);
+
+    if (properties.appendTo !== undefined) {
+        properties.appendTo.appendChild(elm);
+    }
+    if (properties.innerHTML !== undefined) {
+        elm.innerHTML = properties.innerHTML;
+    }
+    if (properties.id !== undefined) {
+        elm.id = properties.id;
+    }
+    if (properties.class !== undefined) {
+        elm.className = properties.class;
+    }
+    if (properties.click !== undefined) {
+        elm.onclick = properties.click;
+    }
+    if (properties.bg !== undefined) {
+        elm.style.background = properties.bg;
+    }
+
+    
+
+    return elm;
+
+
+
+}
